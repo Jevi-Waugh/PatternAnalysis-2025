fe1002f475eeaefdc1eee85e079c14d28627dffa
Jevi Waugh <codeprogramming99@gmail.com>
2025-10-26 23:41:11 +1000

    Updated old class to BioTrainer class

--- recognition/FLAN-T5-Jevi-Waugh/train.py
+++ recognition/FLAN-T5-Jevi-Waugh/train.py
@@ -16,39 +16,55 @@ from dataset import BioDatasetLoader
 from modules import FLAN_T5_LoRA, FLAN_T5_FullFineTuning
 
 
-class FlanT5_Trainer:
-    """Optimised Trainer for FLAN Models"""
-    
-    def __init__(self, model, tokenizer, train_dataset, test_dataset, data_collator,
-                 num_epochs=4, batch_size=16, learning_rate=3e-4,
-                 output_dir="./output", checkpoints_saving=True, device=None):
+class BioTrainer:
+    def __init__(self, model, tokenizer, train_dataset, eval_dataset, data_collator,
+                 learning_rate=5e-4, num_epochs=4, batch_size=16, 
+                 output_dir="./output", device=None, save_checkpoints=True):
         
-        # Use GPU if possible
         self.device = device if device else ('cuda' if torch.cuda.is_available() else 'cpu')
+        self.tokenizer = tokenizer
+        # in case training fails midway through epoch
+        self.save_checkpoints = save_checkpoints
+        self.output_dir = output_dir
+        self.num_epochs = num_epochs
+        
+        # need to control batch size for training time flexibiloty
+        self.batch_size = batch_size
         self.model = model.to(self.device)
         
-        # Default parameters for the model
-        self.tokenizer = tokenizer
-        self.num_epochs = num_epochs
-        self.batch_size = batch_size
-        self.output_dir = output_dir
-        self.checkpoints_saving = checkpoints_saving
-        
-        # Making sure that the directory is created
+        import os
         os.makedirs(output_dir, exist_ok=True)
-        # If we have checkpoints then save them
-        if checkpoints_saving:
-            # Because we want to have a dedicated section so that we don't lose progress
-            self.checkpoints_saving = os.path.join(self.output_dir, "checkpoints")
+        if save_checkpoints: 
+            self.checkpoint_dir = os.path.join(output_dir, "checkpoints")
             os.makedirs(self.checkpoint_dir, exist_ok=True)
             
-        # Create dataloaders
+        # Create dataloaders for both training and evaluationn sets
         self.train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
-        self.test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=data_collator
-        )
+        self.eval_dataloader = DataLoader(eval_dataset, batch_size=batch_size, shuffle=False, collate_fn=data_collator)
+        # Setup optimizer, look if there is another pne better than adam for fine tuning
+        self.optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
         
-        # Seting yp the optimizer
-        # This has to return AutoModelForSeq2SeqLM.from_pretrained(model_name).parameters()
-        # remember
-        # set decay to 0.01 for now
-        self.optimiser = AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+        # Figure out how to set uop schedulaer later
+        
+        # rouge
+        self.rouge = evaluate.load("rouge")
+        
+        self.saved_history = {
+            'train_loss': [],
+            'eval_loss': [],
+            'rouge1': [],
+            'rouge2': [],
+            'rougeLsum': [],
+            'rougeL': [],
+            # Make sure that the generation length is consistent
+            'gen_len': []
+        }
+        
+    def train(self):
+        pass
+    
+    def save_model(self):
+        pass
+    
+    def _save_checpoint(self):
+        pass
